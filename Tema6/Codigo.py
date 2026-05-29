cf8061542078cb52f0cd0bddd734ccd0c0c13dc6
CristhianBGM <24030063@itesa.edu.mx>
2026-05-29 10:46:12 -0600

    Add files via upload

---
+++ Tema6/Codigo.py
@@ -0,0 +1,17 @@
+
+registros = [10.0, 20.0, 30.0]
+tiempos = [80.0, 110.0, 200.0]
+x_estimar = 25.0
+
+
+dif_1_0 = (tiempos[1] - tiempos[0]) / (registros[1] - registros[0])
+dif_1_1 = (tiempos[2] - tiempos[1]) / (registros[2] - registros[1])
+dif_2 = (dif_1_1 - dif_1_0) / (registros[2] - registros[0])
+
+
+tiempo_estimado = tiempos[0] + dif_1_0 * (x_estimar - registros[0]) + dif_2 * (x_estimar - registros[0]) * (x_estimar - registros[1])
+
+
+print("--- Validación de Rendimiento SQL ---")
+print(f"Registros procesados: {x_estimar} mil")
+print(f"Tiempo de respuesta estimado: {tiempo_estimado} ms")
